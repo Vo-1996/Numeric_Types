228c3e6f9214220c6c0046e5e8aa1c9edc9d1a93
Vo-1996 <73895980+Vo-1996@users.noreply.github.com>
2023-03-09 09:08:16 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Numeric_Types
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double length;
+            double width;
+            double area;
+            string input;
+
+            Console.WriteLine("The length of rectangle is: ");
+            input = Console.ReadLine();
+            length = double.Parse(input);
+
+            Console.WriteLine("The width of rectangle is: ");
+            input = Console.ReadLine();
+            width = double.Parse(input);
+
+            area = (length * width);
+            Console.WriteLine("The area of the rectangle is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
